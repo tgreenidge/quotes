2908b2f8f0cea268fa2e25e2e6cde18003cae452
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-06-11 12:47:55 -0700

    add quotes from web

--- src/main/java/quotes/App.java
+++ src/main/java/quotes/App.java
@@ -3,31 +3,101 @@
  */
 package quotes;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class App {
-    protected static Quote[] createJsonObject(String jsonString) {
+    protected Quote[] createJsonObject(String jsonString) {
         //convert the json to a Quote object)
         Gson gson = new GsonBuilder().serializeNulls().create();
         return  gson.fromJson(jsonString, Quote[].class);
     }
 
-        protected static Quote getRandomQuote(String str) {
-            Quote[] quotes = createJsonObject(str);
+    protected Quote getRandomQuote(String str) {
+        Quote[] quotes2 = createJsonObject(str);
+        int randomNumber = (int) Math.floor(Math.random() *  quotes2.length);
 
-            int randomNumber = (int) Math.floor(Math.random() *  quotes.length);
-            return quotes[randomNumber];
-        };
+        return quotes2[randomNumber];
+    }
 
-    public static void main(String[] args) {
-        // read file;
+    protected void saveQuoteToFile(String quoteAsString) {
+
         try {
             BufferedReader reader = new BufferedReader(
-                new FileReader("assets/recentquotes.json"));
+                    new FileReader("assets/recentquotes.json"));
+
+            String line = reader.readLine();
+
+            while(line != null) {
+                if(line == "]")
+                    line.replace("]", ",");
+
+                line = reader.readLine();
+            }
+
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("assets/recentquotes.json", true));
+            writer.append(quoteAsString);
+            //writer.newLine();
+            writer.write(quoteAsString);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getQuoteFromWeb() {
+        // read file;
+        try {
+
+            URL url = new URL("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            // this line of code actually goes to the internet!
+            BufferedReader reader = new BufferedReader(new InputStreamReader((con.getInputStream())));
+            String quoteString = reader.readLine().replace("starWarsQuote", "text");
+
+            int index = quoteString.lastIndexOf("—");
+            String quoteStringWithAuthor;
+
+            if(index == -1) {
+                quoteStringWithAuthor = quoteString;
+            } else {
+                quoteStringWithAuthor = quoteString.substring(0, index) + "\",\"author\":\"" + quoteString.substring(index + 2);
+            }
+
+            Gson gson = new Gson();
+
+            Quote quote = gson.fromJson(quoteStringWithAuthor, Quote.class);
+            //saveQuoteToFile(quoteStringWithAuthor);
+            return quote.getText();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return getQuoteFromFile();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return getQuoteFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return getQuoteFromFile();
+        }
+    }
+
+    public String getQuoteFromFile() {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("assets/recentquotes.json"));
             StringBuilder str = new StringBuilder();
             String quoteString = reader.readLine();
 
@@ -36,11 +106,20 @@ public class App {
                 quoteString = reader.readLine();
             }
 
-            System.out.println(getRandomQuote(str.toString()).toString());
+            return getRandomQuote(str.toString()).toString();
 
         } catch (IOException e) {
             e.printStackTrace();
+            return "Two is the smallest prime number";
         }
+    }
+
+
+
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.getQuoteFromWeb());
+
     }
 
 }
